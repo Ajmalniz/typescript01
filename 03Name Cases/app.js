ff71dbe18533934560d9dbdd84f67b6bc8176d53
Ajmalniz <128520849+Ajmalniz@users.noreply.github.com>
2023-07-09 03:01:55 -0700

    namecase

--- 03Name Cases/app.js
+++ 03Name Cases/app.js
@@ -1,4 +1,12 @@
 var str = "AJmal Khan";
+//there is no buliting method to title case in typescript 
+function convertToTitleCase(inputString) {
+    var words = inputString.toLowerCase().split(" ");
+    for (var i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+    return words.join(" ");
+}
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
-//there is no buliting method to title case in typescript 
+console.log(convertToTitleCase(str));
--- 03Name Cases/app.ts
+++ 03Name Cases/app.ts
@@ -1,5 +1,13 @@
-var str : string = "AJmal Khan" 
+let str : string = "AJmal Khan" 
+//there is no buliting method to title case in typescript 
+function convertToTitleCase(inputString: string): string {
+  const words = inputString.toLowerCase().split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+  }
+  return words.join(" ");
+}
+
 console.log(str.toUpperCase())
 console.log(str.toLowerCase())
-//there is no buliting method to title case in typescript 
- 
+console.log(convertToTitleCase(str))
